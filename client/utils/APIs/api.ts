f6d5ae9ebf6122c73d7c0fcc341c350436e42e8d
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2024-03-01 17:17:29 +0530

    some changes

--- client/utils/APIs/api.ts
+++ client/utils/APIs/api.ts
@@ -1,5 +1,6 @@
-export const MEDICINE_RECOMMENDATION_API = "https://medicinerecommendersystem-5jsx.onrender.com"
-// export const MEDICINE_RECOMMENDATION_API = "https://medicinerecommender.azurewebsites.net"
+// export const MEDICINE_RECOMMENDATION_API = "https://medicinerecommendersystem-5jsx.onrender.com"
+export const MEDICINE_RECOMMENDATION_API = "https://medicinerecommender.azurewebsites.net"
+// export const MEDICINE_RECOMMENDATION_API = "http://127.0.0.1:5000"
 export const KIDNEY_DISEASE_API = "https://kidneydiseaseclassificationbackend.onrender.com/predict";
 export const ORAL_DISEASE_API = "https://oraldiseasedetector.onrender.com/predict";
 export const CANCER_DISEASE_API = "https://chest-cancer-classification-ck5n.onrender.com/predict";
