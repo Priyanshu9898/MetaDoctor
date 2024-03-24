765dcb83dcfed8c07b3e3610be8029bc99506721
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2024-03-24 21:18:16 +0530

    new urls for api added

--- client/app/api/bot/foodAdvisor/route.tsx
+++ client/app/api/bot/foodAdvisor/route.tsx
@@ -118,3 +118,133 @@ export const POST = async (req: NextRequest, res: NextResponse) => {
     );
   }
 };
+
+
+// import axios from "axios";
+// import { NextRequest, NextResponse } from "next/server";
+// import OpenAI from "openai";
+
+// const openai = new OpenAI();
+
+// export const POST = async (req: NextRequest, res: NextResponse) => {
+//   try {
+//     const body = await req.formData();
+    
+//     const image: File | null = body.get("image") as unknown as File;
+//     const mimeType: string | null = body.get("mimeType") as string;
+//     console.log(mimeType);
+
+//     if (!image) {
+//       return NextResponse.json(
+//         {
+//           message: "Please Upload a Image.",
+//           success: false,
+//         },
+//         { status: 401 }
+//       );
+//     }
+//     const bytes = await image.arrayBuffer();
+//     const imageBuffer = Buffer.from(bytes);
+//     const imageBase64String = imageBuffer.toString("base64");
+
+//     // const { image, mimeType } = body;
+
+//     const input_prompt = `
+//     As a recognized expert in nutritional analysis, your task involves meticulously examining food items displayed in an image and calculating their total caloric content accurately. It is imperative that your response adheres strictly to a structured JSON format to ensure the data is precise and easily interpretable. Furthermore, based on your analysis of the food items, you are tasked with creating a comprehensive meal plan that offers balanced and nutritious meal recommendations for each day of the upcoming week. Each suggested meal must align with dietary guidelines and consider an optimal caloric intake to promote health and well-being.
+
+//     Your response must include the following elements:
+//     1. A detailed enumeration of each food item's caloric content as identified from the image.
+//     2. The aggregate caloric value of all the food items combined.
+//     3. A seven-day India strict Vegetarian meal plan that includes recommendations for breakfast, lunch, dinner, and two snacks for each day, designed to maintain a balanced diet.
+
+//     Please ensure your response is formatted as structured JSON, adhering to the example format provided below. Note: The ellipsis ("...") symbol is used here to indicate continuation or the presence of additional items or days in the meal plan. These symbols should not be included in your actual JSON response. Ensure no markdown syntax (such as backticks) or additional characters that would invalidate the JSON format are included in your response.
+
+//     Example format for your JSON response:
+
+//     {
+//       "image_analysis": {
+//         "total_calories": "<Total calculated calories>",
+//         "items": [
+//           {"name": "Item 1", "calories": "<number of calories>"},
+//           {"name": "Item 2", "calories": "<number of calories>"}
+//         ]
+//       },
+//       "weekly_meal_plan": {
+//         "Day 1": {
+//           "breakfast": "<meal suggestion>",
+//           "lunch": "<meal suggestion>",
+//           "dinner": "<meal suggestion>",
+//           "snacks": [
+//             "<snack 1 suggestion>",
+//             "<snack 2 suggestion>"
+//           ]
+//         },
+//         "Day 7": {
+//           "breakfast": "<meal suggestion>",
+//           "lunch": "<meal suggestion>",
+//           "dinner": "<meal suggestion>",
+//           "snacks": [
+//             "<snack 1 suggestion>",
+//             "<snack 2 suggestion>"
+//           ]
+//         }
+//       }
+//     }
+
+//     Please replace placeholders (e.g., "<Total calculated calories>", "<meal suggestion>") with actual data values based on your analysis. The JSON response must be free of syntax errors and should not include characters or formatting that would prevent it from being parsed correctly as valid JSON.
+//     `;
+//     // // Note: When using this prompt in your application, ensure to parse the JSON response correctly and handle any potential exceptions that might arise from invalid JSON syntax.
+
+//     let headers = {
+//       "Content-Type": "application/json",
+//       "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+//     };
+
+    
+
+//     const payload = {
+//       "model": "gpt-4-vision-preview",
+//       "messages": [
+//         {
+//           "role": "user",
+//           "content": [
+//             {
+//               "type": "text",
+//               "text": "What’s in this image?"
+//             },
+//             {
+//               "type": "image_url",
+//               "image_url": {
+//                 "url": `data:${mimeType};base64,${imageBase64String}`
+//               }
+//             }
+//           ]
+//         }
+//       ],
+//       "max_tokens": 2000
+//     };
+
+//     console.log(payload);
+
+//     response = axios.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+//     print(response.json())
+
+//     return NextResponse.json(
+//       {
+//         message: "Response generated successfully",
+//         success: true,
+//         response: finaltext,
+//       },
+//       { status: 201 }
+//     );
+//   } catch (error) {
+//     return NextResponse.json(
+//       { success: false, message: error },
+//       { status: 500 }
+//     );
+//   }
+// };
+
+
+
--- client/utils/APIs/api.ts
+++ client/utils/APIs/api.ts
@@ -1,7 +1,12 @@
 // export const MEDICINE_RECOMMENDATION_API = "https://medicinerecommendersystem-5jsx.onrender.com"
 export const MEDICINE_RECOMMENDATION_API = "https://medicinerecommender.azurewebsites.net"
 // export const MEDICINE_RECOMMENDATION_API = "http://127.0.0.1:5000"
-export const KIDNEY_DISEASE_API = "https://kidneydiseaseclassificationbackend.onrender.com/predict";
-export const ORAL_DISEASE_API = "https://oraldiseasedetector.onrender.com/predict";
-export const CANCER_DISEASE_API = "https://chest-cancer-classification-ck5n.onrender.com/predict";
-export const CALORY_DETECTOR = "/api/bot/foodAdvisor";
+// export const KIDNEY_DISEASE_API = "https://kidneydiseaseclassificationbackend.onrender.com/predict";
+// export const ORAL_DISEASE_API = "https://oraldiseasedetector.onrender.com/predict";
+// export const CANCER_DISEASE_API = "https://chest-cancer-classification-ck5n.onrender.com/predict";
+// export const CALORY_DETECTOR = "/api/bot/foodAdvisor";
+// metadoctorservices.azurewebsites.net
+export const KIDNEY_DISEASE_API = "https://metadoctorservices.azurewebsites.net/predict_kidney_disease";
+export const ORAL_DISEASE_API = "https://metadoctorservices.azurewebsites.net/predict_oral_disease";
+export const CANCER_DISEASE_API = "https://metadoctorservices.azurewebsites.net/predict_cancer_disease";
+export const CALORY_DETECTOR = "https://metadoctorservices.azurewebsites.net/bot/foodAdvisor";
